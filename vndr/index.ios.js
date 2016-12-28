8f3951c92dc10498fde022fe6eced65ba63e29b9
Americool <aberanderson@gmail.com>
2016-12-27 19:21:17 -0500

    added super basic scrolling list view

--- vndr/index.ios.js
+++ vndr/index.ios.js
@@ -9,48 +9,57 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ScrollView
 } from 'react-native';
-import List from './src/List.js'
+import List from './src/List'
+import Header from './src/Header'
 
 export default class vndr extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <List />
+        <View>
+          <Header headerName="List of Vendors" />
+          <ScrollView>
+            <List vndrName="Huey's House of Dogs" description="Food by Dogs for Dogs" distance='.05' />
+            <List vndrName="Abe's Grule Slop" description="Hurried food for desprate people" distance='.07' />
+            <List vndrName="Sandwich Town" description="Baloney Sammiches" distance='1.5' />
+            <List vndrName="Barf Store" description="Puke, etc." distance='10' />
+            <List vndrName="Huey's House of Dogs" description="Food by Dogs for Dogs" distance='.05' />
+            <List vndrName="Abe's Grule Slop" description="Hurried food for desprate people" distance='.07' />
+            <List vndrName="Sandwich Town" description="Baloney Sammiches" distance='1.5' />
+            <List vndrName="Barf Store" description="Puke, etc." distance='10' />
+            <List vndrName="Huey's House of Dogs" description="Food by Dogs for Dogs" distance='.05' />
+            <List vndrName="Abe's Grule Slop" description="Hurried food for desprate people" distance='.07' />
+            <List vndrName="Sandwich Town" description="Baloney Sammiches" distance='1.5' />
+            <List vndrName="Barf Store" description="Puke, etc." distance='10' />
+            <List vndrName="Huey's House of Dogs" description="Food by Dogs for Dogs" distance='.05' />
+            <List vndrName="Abe's Grule Slop" description="Hurried food for desprate people" distance='.07' />
+            <List vndrName="Sandwich Town" description="Baloney Sammiches" distance='1.5' />
+            <List vndrName="Barf Store" description="Puke, etc." distance='10' />
+          </ScrollView>
       </View>
-
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
 
 AppRegistry.registerComponent('vndr', () => vndr);
--- vndr/src/Header.js
+++ vndr/src/Header.js
@@ -3,32 +3,33 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 //make Component
-const List = (props) => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
 
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>{ props.vndrName } | {props.description} | {props.distance} mi.</Text>
+      <Text style={textStyle}>{ props.headerName }</Text>
     </View>
   );
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8',
+    backgroundColor: '#daa520',
     justifyContent: 'center',
     alignItems: 'center',
     height: 60,
+    paddingTop: 15,
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2, },
-    shadowOpacity: 0.2,
+    shadowOffset: { width: 2, height: 2, },
+    shadowOpacity: 0.9,
     elevation: 2,
     position: 'relative'
   },
   textStyle: {
-    fontSize: 12
+    fontSize: 28
   }
 };
 
 // make the Component available to rest of application
-export default List;
+export default Header;
