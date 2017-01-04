00ed6eb8ddb91dfd809587a762497e44e98519d7
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-01-03 21:43:34 -0800

    adds first test

---
+++ vndr/src/List.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import List from './List';
+import { shallow } from 'enzyme';
+
+const dummyVendor = {
+  vndrName: 'fake',
+  description: 'even faker',
+  distance: 2,
+}
+
+it('renders without crashing', () => {
+  const wrapper = shallow (<List vendor={dummyVendor} />);
+  expect(wrapper).toExist;
+});
