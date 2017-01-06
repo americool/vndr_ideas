9bb14f4f0c6209b940dd39910cc26d289dc29434
Americool <aberanderson@gmail.com>
2017-01-06 00:29:02 -0500

    some extra package/yarn stuff

--- src/test/List.test.js
+++ src/test/List.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import List from '../List';
 import { shallow } from 'enzyme';
+import List from '../List';
+
 
 const dummyVendor = {
   vndrName: 'fake',
   description: 'even faker',
   distance: 2,
-}
+};
 
 it('renders without crashing', () => {
   const wrapper = shallow (<List vendor={dummyVendor} />);
